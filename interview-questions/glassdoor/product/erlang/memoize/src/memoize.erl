e28191ffe5cdb0d10a5cd79def570bf0a3b23970
Todd Greenwood-Geer <toddg@zwrob.com>
2019-06-08 13:01:40 -0700

    product : add dump_graph()

--- interview-questions/glassdoor/product/erlang/memoize/src/memoize.erl
+++ interview-questions/glassdoor/product/erlang/memoize/src/memoize.erl
@@ -45,20 +45,27 @@ multm(L) when is_list(L) ->
 memom(L) ->
     memo(lists:reverse(L), #graph{fx=fun(A,B) -> multiply(A,B) end}).
 
-memo([H|T], G) when is_record(G, graph)->
+memo([H|T], G) when is_record(G, graph) andalso is_number(H) ->
+    dump_graph(G),
     N = #node{key=[H], value=H},
-    memo(T, add(N, G));
+    G1 = add(N, G),
+    dump_graph(G1),
+    memo(T, G1);
 memo([], G) -> G.
 
 add(N, G) when is_record(N, node) andalso 
                is_record(G, graph) ->
-    G1 = G#graph{dict=dict:store(N#node.key, N, G#graph.dict)},
-    ?debugFmt("~nadded node: ~p, graph: ~p~n", [N, dict:fetch_keys(G1#graph.dict)]),
+    D0 = G#graph.dict,
+    D1 = dict:store(N#node.key, N, D0),
+    G1 = G#graph{dict=D1},
+    ?debugFmt("~nadded node: ~p~n",[N]),
+    %dump_graph(G1),
     case G1#graph.root of
-        [] -> G1#graph{root=[N]};
-        [Sib|_] -> 
-            G2 = G1#graph{root=[N|G1#graph.root]},
-            create_child(N, Sib, G2)
+        [Sib|_] ->
+            G2 = create_child(N, Sib, G1),
+            G2#graph{root=[N|G2#graph.root]};
+        [] -> 
+            G1#graph{root=[N|G1#graph.root]}
     end.
 
 create_child(nil, nil, G) when is_record(G, graph) ->
@@ -72,14 +79,25 @@ create_child(N1, nil, G) when is_record(N1, node) andalso
 create_child(N1, N2, G) when is_record(N1, node) andalso 
                              is_record(N2, node) andalso 
                              is_record(G, graph) ->
-    ?debugFmt("~ncreate child n1: ~p, n2: ~p, graph: ~p~n", [N1, N2, dict:fetch_keys(G#graph.dict)]),
+    ?debugFmt("~ncreate child n1: ~p, n2: ~p~n", [N1, N2]),
+    %dump_graph(G),
     Child = child(N1, N2, G),
     ?debugFmt("~nChild: ~p~n", [Child]),
     D = G#graph.dict,
     D1 = dict:store(Child#node.key, Child, D),
     D2 = dict:update(N1#node.key, fun(Old) -> Old#node{rc=Child#node.key} end, #node{}, D1),
     D3 = dict:update(N2#node.key, fun(Old) -> Old#node{lc = Child#node.key} end, #node{}, D2),
-    create_child(N1, N2#node.rc, G#graph{dict=D3}).
+    G1 = G#graph{dict=D3},
+    ?debugFmt("~nadded child: ~p~n", [Child]),
+    %dump_graph(G1),
+    case N2#node.rc of
+        nil -> 
+            G1;
+        NextSibKey ->
+            NextSib = dict:fetch(NextSibKey, G1#graph.dict),
+            create_child(N1, NextSib, G1)
+    end.
+
 
 child(N1,N2,G) when is_record(N1, node) andalso 
                     is_record(N2, node) andalso 
@@ -96,14 +114,17 @@ fetch(K, G) when is_record(G, graph) ->
     N#node.value.
 
 multiply(A,B) when is_number(A) andalso is_number(B) -> 
-    ?debugFmt("~nA: ~p, B: ~p~n", [A, B]),
+    ?debugFmt("~nmultiply ~w * ~w ~n", [A, B]),
     A*B.
+
+dump_graph(G) when is_record(G, graph) ->
+    ?debugFmt("~nGraph START--------------------------------~n\troot: ~p~n\tfx: ~p~n\tdict: ~n~w~nGraph END----------------------------------", [G#graph.root, G#graph.fx, dict:to_list(G#graph.dict)]).
 
 %%====================================================================
 %% Eunit tests
 %%====================================================================
 
-memo_0_test_() ->
+memom_1_test_() ->
     [
      ?_assertEqual(fetch([1], memom([1])), 1),
      ?_assertEqual(fetch([2], memom([2])), 2),
@@ -111,7 +132,7 @@ memo_0_test_() ->
      ?_assertEqual(fetch([999], memom([999])), 999)
     ].
 
-memo_1_test_() ->
+memom_2_test_() ->
     M = memom([2,3]),
     [
      ?_assertEqual(fetch([2], M), 2),
@@ -119,3 +140,25 @@ memo_1_test_() ->
      ?_assertEqual(fetch([2,3], M), 6)
     ].
 
+%memom_3_test_() ->
+%    M = memom([8,9,2]),
+%    [
+%     ?_assertEqual(fetch([8], M), 8),
+%     ?_assertEqual(fetch([9], M), 9),
+%     ?_assertEqual(fetch([2], M), 2),
+%     ?_assertEqual(fetch([8,9], M), 8*9),
+%     ?_assertEqual(fetch([9,2], M), 9*2),
+%     ?_assertEqual(fetch([8,9,2], M), 8*9*2)
+%    ].
+%%
+%
+%memo_2_test_() ->
+%    M = memom([2,3,1,0,100,9]),
+%    [
+%     ?_assertEqual(fetch([2], M), 2),
+%     ?_assertEqual(fetch([3], M), 3),
+%     ?_assertEqual(fetch([2,3], M), 6),
+%     ?_assertEqual(fetch([2,3,1,0], M), 0),
+%     ?_assertEqual(fetch([2,3,1], M), 6),
+%     ?_assertEqual(fetch([100,9], M), 900)
+%    ].
